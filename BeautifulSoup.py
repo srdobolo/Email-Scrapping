ffbcb6baaa43831d18e758453e793b7ad75b53fe
João Lima <149976201+srdobolo@users.noreply.github.com>
2024-05-30 21:09:38 +0100

    Update BeautifulSoup.py

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests, re
 
 email_id = re.compile('\w+@\w+.\w+')
-html = requests.get('https://netemprego.com/view.php?job_id=2010610&auth_sess=e2t7rcudbcepcub7fsja4slbvr&ref=1e73e70e9b7894247ffa7f4d7').text
+html = requests.get('SITE URL').text
 soup = BeautifulSoup(html, 'html.parser')
 emails = soup.find_all(text=email_id)
 
